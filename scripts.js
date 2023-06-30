c8da20b20a6c6a9ccfc8017e1ca5aa95de7551a2
cody <phamgcody01@gmail.com>
2023-06-30 12:28:51 -0400

    Make gameboard show symbols on array and DOM

--- scripts.js
+++ scripts.js
@@ -1,7 +1,87 @@
 const gameboard = (() => {
-    const board = [['x', 'x', 'x'], ['x', 'x', 'x'], ['x', 'x', 'x']];
+    const board = [['', '', ''], ['', '', ''], ['', '', '']];
     return {
         board
     };
 })();
+
+const playerFactory = (symbol) => {
+    symbol;
+    return {
+        symbol
+    }
+}
+
+const game = (() => {
+    let user = playerFactory('X');
+    let friend = playerFactory('O');
+    let computer = playerFactory('O');
+    let turn = true;
+    let cubes = document.querySelectorAll('.cube');
+
+    const boardCheck = (turn, e) => {
+        if (e.target.id <= 3) {
+            if (turn) {
+                gameboard.board[0][e.target.id-1] = user.symbol;
+            }
+            else {
+                gameboard.board[0][e.target.id-1] = friend.symbol;
+            }
+        }
+        else if (e.target.id <= 6) {
+            if (turn) {
+                gameboard.board[1][e.target.id-4] = user.symbol;
+            }
+            else {
+                gameboard.board[1][e.target.id-4] = friend.symbol;
+            }
+        }
+        else {
+            if (turn) {
+                gameboard.board[2][e.target.id-7] = user.symbol;
+            }
+            else {
+                gameboard.board[2][e.target.id-7] = friend.symbol;
+            }
+
+        }
+    }
+
+    const drawOnBoard = () => {Array.from(cubes).forEach((cube) => {
+        cube.addEventListener('click', (e) => {
+            if (turn) {
+                e.target.textContent = user.symbol;
+                boardCheck(turn, e);
+                turn = false;
+            }
+
+            else {
+                e.target.textContent = friend.symbol;
+                boardCheck(turn, e);
+                turn = true;
+            }
+
+            cube.disabled = true;
+            console.log(gameboard.board);
+        })
+    })}
+
+
+    return {
+        drawOnBoard,
+    };
+})();
+
+game.drawOnBoard();
+
+
+//
+
+
+
+
+
+
+
+//
 
